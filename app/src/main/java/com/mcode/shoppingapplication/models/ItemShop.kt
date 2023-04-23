dadc22d1fc8cb44a6e3093e56096fe747dd9c976
Monica <moni21889@gmail.com>
2023-04-23 12:00:30 -0500

    Creación de modelos

---
+++ app/src/main/java/com/mcode/shoppingapplication/models/ItemShop.kt
@@ -0,0 +1,6 @@
+package com.mcode.shoppingapplication.models
+
+data class ItemShop(
+    val product: Product,
+    var count: Int
+)
---
+++ app/src/main/java/com/mcode/shoppingapplication/models/Product.kt
@@ -0,0 +1,8 @@
+package com.mcode.shoppingapplication.models
+
+data class Product (
+    val id: Int,
+    val name: String,
+    val description: String,
+    val price: Int
+        )
