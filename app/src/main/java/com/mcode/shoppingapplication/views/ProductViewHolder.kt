5d128cc726823b52cc4347b19d5249f65332576d
Monica <moni21889@gmail.com>
2023-04-24 13:49:57 -0500

    Se corrige el mmodelo y adapter para mostrar la información de cada producto

--- app/src/main/java/com/mcode/shoppingapplication/views/ProductViewHolder.kt
+++ app/src/main/java/com/mcode/shoppingapplication/views/ProductViewHolder.kt
@@ -1,13 +1,26 @@
 package com.mcode.shoppingapplication.views
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.mcode.shoppingapplication.databinding.ViewholderProductItemBinding
 import com.mcode.shoppingapplication.models.ItemShop
+import java.text.DecimalFormat
 
 class ProductViewHolder(var binding: ViewholderProductItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    val dec = DecimalFormat("#,###,###")
 
     fun bindData(product: ItemShop){
         binding.item = product
+        binding.priceWithFormat = dec.format(product.product.price)
+        val count = product.count
+        if(count == 0){
+            binding.imageviewBuy.visibility = View.VISIBLE
+            binding.textviewCount.visibility = View.INVISIBLE
+        }else{
+            binding.imageviewBuy.visibility = View.INVISIBLE
+            binding.textviewCount.visibility = View.VISIBLE
+            binding.textviewCount.text = count.toString()
+        }
         binding.executePendingBindings()
     }
 }
